794b9d9dfe4e4bf59d796f4dde82d4915bc80b72
Nori Parelius <nori.parelius@protonmail.com>
2024-07-12 23:55:37 +0200

    Add back org-babel

--- init.el
+++ init.el
@@ -5,3 +5,16 @@
   (load "~/.emacs.d/home.el"))
  ((equal system-name "MU386U")
   (load "C:/Users/ELPAR/AppData/Roaming/.emacs.d/work.el")))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(mixed-pitch vertico ox-hugo orderless marginalia magit languagetool embark ef-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

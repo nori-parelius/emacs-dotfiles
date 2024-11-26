a459d33461d28900d5418cd167619df283e579fa
Nori Parelius <eleonora-jonasova.parelius@ffi.no>
2024-11-26 14:05:11 +0100

    Merge CompNotes with Notes

--- autopull-work.bat
+++ autopull-work.bat
@@ -1,6 +1,4 @@
 @echo off
-cd "C:\Users\ELPAR\OneDrive - Forsvarets forskningsinstitutt\_Documents\Reference System\CompNotes"
-git pull origin master
 
 cd "C:\Users\ELPAR\AppData\Roaming\.emacs.d"
 git pull origin master
--- autopush-work.bat
+++ autopush-work.bat
@@ -4,28 +4,7 @@ REM Get date time
 for /f "tokens=1-4 delims=/ " %%a in ('date /t') do (set mydate=%%a-%%b-%%c)
 for /f "tokens=1-2 delims=: " %%a in ('time /t') do (set mytime=%%a-%%b)
 
-REM CompNotes first
-cd "C:\Users\ELPAR\OneDrive - Forsvarets forskningsinstitutt\_Documents\Reference System\CompNotes"
-
-git status --porcelain
-IF NOT ERRORLEVEL 1 (
-    echo Changes detected, staging changes...
-    git add -A
-
-
-
-    REM Commit changes with a message
-    echo Commiting changes...
-    git commit -m "Automated commit on %mydate% at %mytime%"
-) ELSE (
-    echo No changes to commit.
-)
-
-REM Push changes to the remote repository
-echo Pushing changes...
-git push origin master
-
-REM Then .emacs.d
+REM .emacs.d first
 cd "C:\Users\ELPAR\AppData\Roaming\.emacs.d"
 echo %cd%
 

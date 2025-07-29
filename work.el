36caef4c2c8cf293bfcdb7d379b7c30d08a30ee2
Nori Parelius <nori.parelius@protonmail.com>
2025-07-29 23:19:18 +0200

    Remove the special per session backup, it slows things?

--- work.el
+++ work.el
@@ -49,21 +49,21 @@
 
 ;; Default and per-save backups go here:
 (setq backup-directory-alist '(("" . "~/.emacs.d/backup/per-save")))
-(defun nori-force-backup-of-buffer ()
-  ;; Make a special "per session" backup at the first save of each
-  ;; emacs session.
-  (when (not buffer-backed-up)
-    ;; Override the default parameters for per-session backups.
-    (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
-          (kept-new-versions 3))
-      (backup-buffer)))
-  ;; Make a "per save" backup on each save.  The first save results in
-  ;; both a per-session and a per-save backup, to keep the numbering
-  ;; of per-save backups consistent.
-  (let ((buffer-backed-up nil))
-    (backup-buffer)))
-
-(add-hook 'before-save-hook  'nori-force-backup-of-buffer)
+;;(defun nori-force-backup-of-buffer ()
+;;  ;; Make a special "per session" backup at the first save of each
+;;  ;; emacs session.
+;;  (when (not buffer-backed-up)
+;;    ;; Override the default parameters for per-session backups.
+;;    (let ((backup-directory-alist '(("" . "~/.emacs.d/backup/per-session")))
+;;          (kept-new-versions 3))
+;;      (backup-buffer)))
+;;  ;; Make a "per save" backup on each save.  The first save results in
+;;  ;; both a per-session and a per-save backup, to keep the numbering
+;;  ;; of per-save backups consistent.
+;;  (let ((buffer-backed-up nil))
+;;    (backup-buffer)))
+;;
+;;(add-hook 'before-save-hook  'nori-force-backup-of-buffer)
 ;;(setq org-capture-templates
 ;;      '(("i" "Inbox Entry" entry
 ;;	 (file+headline "~/Documents/Notes/0000--entry-point.org" "Inbox")
